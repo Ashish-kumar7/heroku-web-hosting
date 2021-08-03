9b652292f6fadaffaa898632563cb3d79b734af1
ashish007kumar <ashishkumar357ak@gmail.com>
2021-08-03 20:37:55 +0530

    initial commit

--- js/client.js
+++ js/client.js
@@ -1,4 +1,4 @@
-const socket=io('http://localhost:8000');
+const socket=io('http://localhost:9002');
 
 const form=document.getElementById('send-container');
 const messageInput=document.getElementById('messageInp');
